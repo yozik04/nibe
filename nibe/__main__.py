df209c3e5287d84498444d2e5f42e19cbc40b905
Joakim Plate <elupus@ecce.se>
2022-09-26 22:41:29 +0200

    Add hacked cli

---
+++ nibe/__main__.py
@@ -0,0 +1,187 @@
+from __future__ import annotations
+
+import asyncclick as click
+import logging
+import asyncio
+from ast import literal_eval
+from typing import IO
+import io
+
+from construct import Struct, Const, Int8ul, RawCopy, Select, GreedyRange
+from .heatpump import HeatPump, Model
+from .connection.nibegw import NibeGW
+from .connection.nibegw import Request, Response
+
+from .coil import Coil
+
+
+Ack = Struct(
+    "fields" / RawCopy(
+        Struct(
+            "Ack" / Const(0x06, Int8ul)
+        )
+    )
+)
+
+Nak = Struct(
+    "fields" / RawCopy(
+        Struct(
+            "Ack" / Const(0x15, Int8ul)
+        )
+    )
+)
+
+
+Block = Select(
+    Response,
+    Request,
+    Ack,
+    Nak,
+)
+
+Stream = GreedyRange(Block)
+
+
+
+@click.group()
+async def cli():
+    pass
+
+_global_options = [
+    click.argument("remote_ip", type=str),
+    click.option("--listening_ip", type=str),
+    click.option("--listening_port", type=int, default=10090),
+    click.option("--remote_read_port", type=int, default=10091),
+    click.option("--remote_write_port", type=int, default=10092),
+    click.option(
+        "--model",
+        type=click.Choice([model.name for model in Model]),
+        default=Model.F1155.name,
+    ),
+    click.option("-v", "--verbose", count=True)
+
+]
+
+
+def global_options(func):
+    for option in reversed(_global_options):
+        func = option(func)
+    return func
+
+
+async def global_setup(
+    remote_ip: str,
+    listening_port: int,
+    listening_ip: str | None,
+    remote_read_port: int,
+    remote_write_port: int,
+    model: str,
+    verbose: int
+):
+    if verbose == 0:
+        log_level = logging.WARNING
+    elif verbose == 1:
+        log_level = logging.INFO
+    else:
+        log_level = logging.DEBUG
+
+    logging.basicConfig(
+        format="[%(levelname)-8s] %(message)s",
+        level=log_level,
+    )
+    logging.log(logging.INFO, "Log level set to %r", log_level)
+
+
+    heatpump = HeatPump(Model[model])
+    heatpump.initialize()
+    connection = NibeGW(
+        heatpump=heatpump,
+        remote_ip=remote_ip,
+        listening_port=listening_port,
+        listening_ip=listening_ip,
+        remote_read_port=remote_read_port,
+        remote_write_port=remote_write_port,
+    )
+    await connection.start()
+
+    return heatpump, connection
+
+
+@cli.command()
+@global_options
+async def monitor(**kwargs):
+    heatpump, _ = await global_setup(**kwargs)
+
+    def on_coil_update(coil: Coil):
+        click.echo(f"{coil.name}: {coil.value}")
+
+    heatpump.subscribe(HeatPump.COIL_UPDATE_EVENT, on_coil_update)
+
+    while True:
+        await asyncio.sleep(1)
+
+
+@cli.command()
+@global_options
+@click.argument("parameter", type=int)
+async def read(parameter: int, **kwargs):
+    heatpump, connection = await global_setup(**kwargs)
+
+    coil = heatpump.get_coil_by_address(parameter)
+    click.echo(await connection.read_coil(coil))
+
+
+@cli.command()
+@global_options
+@click.argument("parameter", type=int)
+@click.argument("value", type=str)
+async def write(parameter: int, value: str, **kwargs):
+    heatpump, connection = await global_setup(**kwargs)
+
+    coil = heatpump.get_coil_by_address(parameter)
+    if coil.mappings:
+        coil.value = value
+    else:
+        coil.value = float(value)
+    click.echo(await connection.write_coil(coil))
+
+
+@cli.command()
+@click.argument("data", type=str)
+@click.option("--type", type=click.Choice(["hex", "bytes"]), default="hex")
+async def parse_request(data: str, type: str):
+    if type == "hex":
+        raw = bytes.fromhex(data)
+    elif type == "bytes":
+        raw = bytes(literal_eval(data))
+    request = Request.parse(raw)
+    click.echo(request)
+
+
+def read_bytes_socat(file: IO):
+    lines: list[str] = file.readlines()
+
+    for line in lines:
+        if line.startswith("> "):
+            continue
+        yield from bytes.fromhex(line)
+
+
+@cli.command()
+@click.argument("file", type=click.File())
+def parse_file(file: IO):
+
+    with io.BytesIO(bytes(read_bytes_socat(file))) as stream:
+        everything = Stream.parse_stream(stream)
+        def values():
+            for packet in everything:
+                yield str(packet.fields.value) + "\n"
+        click.echo_via_pager(values())
+
+        remaining = stream.read()
+        if remaining:
+            click.echo(f"Remaining: {stream.read()}") 
+try:
+    cli()
+except (KeyboardInterrupt, SystemExit):
+    pass
