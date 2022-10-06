78ca00fbed0cd001fffe3bad110f3d09c45d4061
Joakim Plate <elupus@ecce.se>
2022-10-06 22:07:33 +0200

    Allow batching coils in api request

--- nibe/connection/__init__.py
+++ nibe/connection/__init__.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from typing import AsyncGenerator, Iterable
 
 from nibe.coil import Coil
 from nibe.heatpump import ProductInfo
@@ -17,6 +18,12 @@ class Connection(ABC):
     @abstractmethod
     async def read_coil(self, coil: Coil, timeout: float = DEFAULT_TIMEOUT) -> Coil:
         pass
+
+    async def read_coils(
+        self, coils: Iterable[Coil], timeout: float = DEFAULT_TIMEOUT
+    ) -> AsyncGenerator[Coil]:
+        for coil in coils:
+            yield await self.read_coil(coil, timeout)
 
     @abstractmethod
     async def write_coil(self, coil: Coil, timeout: float = DEFAULT_TIMEOUT) -> Coil:
