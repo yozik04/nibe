0ff648c673cf09d4f2d448d7e4fc1ca2275a4096
Joakim Plate <elupus@ecce.se>
2022-10-10 20:24:49 +0200

    Add more test

--- tests/connection/test_modbus.py
+++ tests/connection/test_modbus.py
@@ -1,8 +1,10 @@
+from typing import List, Union
 from unittest.mock import AsyncMock, patch
 
 from async_modbus import AsyncClient
 import pytest
 
+from nibe.coil import Coil
 from nibe.connection.modbus import Modbus
 from nibe.heatpump import HeatPump, Model
 
@@ -15,7 +17,7 @@ def fixture_modbus_client():
         yield client
 
 
-@pytest.fixture(name="heatpump")
+@pytest.fixture(name="heatpump", scope="module")
 def fixture_heatpump():
     heatpump = HeatPump(Model.S1255)
     heatpump.initialize()
@@ -27,10 +29,129 @@ def fixture_connection(heatpump: HeatPump):
     yield Modbus(heatpump, "tcp://127.0.0.1", 0)
 
 
-async def test_read_s32_coil(
-    heatpump: HeatPump, connection: Modbus, modbus_client: AsyncMock
+@pytest.mark.parametrize(
+    ("size", "raw", "value"),
+    [
+        ("u32", [b"\x01\x00", b"\x00\x00"], 1),
+        ("u16", [b"\x01\x00"], 1),
+        ("u8", [b"\x01\x00"], 1),
+    ],
+)
+async def test_read_holding_register_coil(
+    connection: Modbus,
+    modbus_client: AsyncMock,
+    size: str,
+    raw: List[bytes],
+    value: Union[int, float, str],
 ):
-    coil = heatpump.get_coil_by_address(40011)
-    modbus_client.read_holding_registers.return_value = [b"\x00\x00", b"\x00\x00"]
+    coil = Coil(40001, "test", "test", size, 1)
+    modbus_client.read_holding_registers.return_value = raw
     coil = await connection.read_coil(coil)
-    assert coil.value == 0
+    assert coil.value == value
+    modbus_client.read_holding_registers.assert_called()
+
+
+@pytest.mark.parametrize(
+    ("size", "raw", "value"),
+    [
+        ("u32", [b"\x01\x00", b"\x00\x00"], 1),
+        ("u16", [b"\x01\x00"], 1),
+        ("u8", [b"\x01\x00"], 1),
+    ],
+)
+async def test_write_holding_register(
+    connection: Modbus,
+    modbus_client: AsyncMock,
+    size: str,
+    raw: List[bytes],
+    value: Union[int, float, str],
+):
+    coil = Coil(40002, "test", "test", size, 1, write=True)
+    coil.value = value
+    coil = await connection.write_coil(coil)
+    modbus_client.write_registers.assert_called_with(
+        slave_id=0, starting_address=1, values=raw
+    )
+
+
+@pytest.mark.parametrize(
+    ("size", "raw", "value"),
+    [
+        ("u32", [b"\x01\x00", b"\x00\x00"], 1),
+        ("u16", [b"\x01\x00"], 1),
+        ("u8", [b"\x01\x00"], 1),
+    ],
+)
+async def test_read_input_register_coil(
+    connection: Modbus,
+    modbus_client: AsyncMock,
+    size: str,
+    raw: List[bytes],
+    value: Union[int, float, str],
+):
+    coil = Coil(30001, "test", "test", size, 1)
+    modbus_client.read_input_registers.return_value = raw
+    coil = await connection.read_coil(coil)
+    assert coil.value == value
+    modbus_client.read_input_registers.assert_called()
+
+
+@pytest.mark.parametrize(
+    ("size", "raw", "value"),
+    [
+        ("u8", [b"\x01"], 1),
+    ],
+)
+async def test_read_discrete_input_coil(
+    connection: Modbus,
+    modbus_client: AsyncMock,
+    size: str,
+    raw: List[bytes],
+    value: Union[int, float, str],
+):
+    coil = Coil(10001, "test", "test", size, 1)
+    modbus_client.read_discrete_inputs.return_value = raw
+    coil = await connection.read_coil(coil)
+    assert coil.value == value
+    modbus_client.read_discrete_inputs.assert_called()
+
+
+@pytest.mark.parametrize(
+    ("size", "raw", "value"),
+    [
+        ("u8", [b"\x01"], 1),
+    ],
+)
+async def test_read_coil_coil(
+    connection: Modbus,
+    modbus_client: AsyncMock,
+    size: str,
+    raw: List[bytes],
+    value: Union[int, float, str],
+):
+    coil = Coil(1, "test", "test", size, 1)
+    modbus_client.read_coils.return_value = raw
+    coil = await connection.read_coil(coil)
+    assert coil.value == value
+    modbus_client.read_coils.assert_called()
+
+
+@pytest.mark.parametrize(
+    ("size", "raw", "value"),
+    [
+        ("u8", [b"\x01"], 1),
+    ],
+)
+async def test_write_coil_coil(
+    connection: Modbus,
+    modbus_client: AsyncMock,
+    size: str,
+    raw: List[bytes],
+    value: Union[int, float, str],
+):
+    coil = Coil(2, "test", "test", size, 1, write=True)
+    coil.value = value
+    coil = await connection.write_coil(coil)
+    modbus_client.write_coils.assert_called_with(
+        slave_id=0, starting_address=1, values=raw
+    )
