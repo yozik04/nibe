a3c1b2c67dcda1a365bba3de6d69bb44218e9008
Joakim Plate <elupus@ecce.se>
2022-11-08 01:12:25 +0100

    Add method to verify connectivity

--- nibe/connection/__init__.py
+++ nibe/connection/__init__.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 from collections.abc import AsyncIterator, Iterable
 
 from nibe.coil import Coil
-from nibe.heatpump import ProductInfo
+from nibe.heatpump import HeatPump, ProductInfo, Series
 
 DEFAULT_TIMEOUT: float = 5
 READ_PRODUCT_INFO_TIMEOUT: float = 20  # Product info message is sent every 15 seconds
@@ -35,3 +35,28 @@ class Connection(ABC):
         raise NotImplementedError(
             "read_product_info method is not implemented for this connection method"
         )
+
+    @abstractmethod
+    async def verify_connectivity(self):
+        pass
+
+
+async def verify_connectivity_read_write_alarm(
+    connection: Connection, heatpump: HeatPump
+):
+    """Verify that we have functioning communication.
+
+    To verify connection, we read the alarm reset field and write it as 0
+    this will be ignored by the pump, this will throw exceptions on failure.
+    """
+    if heatpump.series == Series.S:
+        coil = heatpump.get_coil_by_name("reset-alarm-40023")
+    else:
+        coil = heatpump.get_coil_by_name("alarm-reset-45171")
+
+    coil = await connection.read_coil(coil)
+    value: str | int = 0
+    if coil.mappings:
+        value = coil.mappings[str(value)]
+    coil.value = value
+    coil = await connection.write_coil(coil)
