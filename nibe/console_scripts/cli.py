84bc6a2572a36d4e63a213b2de8e45b7e8115955
Joakim Plate <elupus@ecce.se>
2022-11-11 15:12:28 +0100

    Improve cli syntax

--- nibe/console_scripts/cli.py
+++ nibe/console_scripts/cli.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from ast import literal_eval
 import asyncio
+from contextlib import AbstractAsyncContextManager
 import io
 import logging
 from typing import IO
@@ -9,15 +10,17 @@ from typing import IO
 import asyncclick as click
 from construct import Const, GreedyRange, Int8ul, RawCopy, Select, Struct, Terminated
 
+from nibe.connection import Connection
+
 from ..coil import Coil
 from ..connection.modbus import Modbus
 from ..connection.nibegw import NibeGW, Request, Response
+from ..exceptions import NibeException
 from ..heatpump import HeatPump, Model
 
 Ack = Struct("fields" / RawCopy(Struct("Ack" / Const(0x06, Int8ul))))
 
 Nak = Struct("fields" / RawCopy(Struct("Nak" / Const(0x15, Int8ul))))
-
 
 Block = Select(
     Terminated,
@@ -31,46 +34,8 @@ Stream = GreedyRange(Block)
 
 
 @click.group()
-async def cli():
-    pass
-
-
-_global_options = [
-    click.argument("remote_ip", type=str),
-    click.option(
-        "--remote_type", type=click.Choice(["nibegw", "modbus"]), default="nibegw"
-    ),
-    click.option("--listening_ip", type=str),
-    click.option("--listening_port", type=int, default=10090),
-    click.option("--remote_read_port", type=int, default=10091),
-    click.option("--remote_write_port", type=int, default=10092),
-    click.option("--slave_id", type=int, default=1),
-    click.option(
-        "--model",
-        type=click.Choice([model.name for model in Model]),
-        default=Model.F1155.name,
-    ),
-    click.option("-v", "--verbose", count=True),
-]
-
-
-def global_options(func):
-    for option in reversed(_global_options):
-        func = option(func)
-    return func
-
-
-async def global_setup(
-    remote_ip: str,
-    remote_type: str,
-    listening_port: int,
-    listening_ip: str | None,
-    remote_read_port: int,
-    remote_write_port: int,
-    model: str,
-    verbose: int,
-    slave_id: int,
-):
+@click.option("-v", "--verbose", count=True)
+async def cli(verbose: int):
     if verbose == 0:
         log_level = logging.WARNING
     elif verbose == 1:
@@ -84,74 +49,136 @@ async def global_setup(
     )
     logging.log(logging.INFO, "Log level set to %r", log_level)
 
+
+class ConnectionContext(AbstractAsyncContextManager):
+    def __init__(self, heatpump: HeatPump, connection: Connection) -> None:
+        self.heatpump = heatpump
+        self.connection = connection
+
+    async def __aenter__(self) -> ConnectionContext:
+        await self.connection.start()
+        return self
+
+    async def __aexit__(self, __exc_type, __exc_value, __traceback):
+        await self.connection.stop()
+
+
+@cli.group(chain=True)
+@click.argument("remote_ip", type=str)
+@click.option("--listening_ip", type=str)
+@click.option("--listening_port", type=int, default=10090)
+@click.option("--remote_read_port", type=int, default=10091)
+@click.option("--remote_write_port", type=int, default=10092)
+@click.option(
+    "--model",
+    type=click.Choice([model.name for model in Model]),
+    default=Model.F1155.name,
+)
+@click.pass_context
+async def nibegw(
+    ctx: click.Context,
+    remote_ip: str,
+    listening_port: int,
+    listening_ip: str | None,
+    remote_read_port: int,
+    remote_write_port: int,
+    model: str,
+):
+
     heatpump = HeatPump(Model[model])
     await heatpump.initialize()
-    if remote_type == "nibegw":
-        connection = NibeGW(
-            heatpump=heatpump,
-            remote_ip=remote_ip,
-            listening_port=listening_port,
-            listening_ip=listening_ip,
-            remote_read_port=remote_read_port,
-            remote_write_port=remote_write_port,
-        )
-    elif remote_type == "modbus":
-        connection = Modbus(
-            heatpump=heatpump,
-            url=f"tcp://{remote_ip}:{remote_read_port}",
-            slave_id=slave_id,
-        )
+    connection = NibeGW(
+        heatpump=heatpump,
+        remote_ip=remote_ip,
+        listening_port=listening_port,
+        listening_ip=listening_ip,
+        remote_read_port=remote_read_port,
+        remote_write_port=remote_write_port,
+    )
 
-    await connection.start()
-
-    return heatpump, connection
+    ctx.obj = await ctx.with_async_resource(ConnectionContext(heatpump, connection))
 
 
-@cli.command()
-@global_options
-async def monitor(**kwargs):
-    heatpump, _ = await global_setup(**kwargs)
+@cli.group(chain=True)
+@click.argument("remote_ip", type=str)
+@click.option("--remote_port", type=int, default=502)
+@click.option("--slave_id", type=int, default=1)
+@click.option(
+    "--model",
+    type=click.Choice([model.name for model in Model]),
+    default=Model.F1155.name,
+)
+@click.pass_context
+async def modbus(
+    ctx: click.Context, remote_ip: str, remote_port: str, model: str, slave_id: int
+):
 
+    heatpump = HeatPump(Model[model])
+    await heatpump.initialize()
+    connection = Modbus(
+        heatpump=heatpump,
+        url=f"tcp://{remote_ip}:{remote_port}",
+        slave_id=slave_id,
+    )
+
+    ctx.obj = await ctx.with_async_resource(ConnectionContext(heatpump, connection))
+
+
+def add_connect_command(command: click.Command):
+    nibegw.add_command(command)
+    modbus.add_command(command)
+
+
+@click.command()
+@click.pass_obj
+async def monitor(obj: ConnectionContext):
     def on_coil_update(coil: Coil):
         click.echo(f"{coil.name}: {coil.value}")
 
-    heatpump.subscribe(HeatPump.COIL_UPDATE_EVENT, on_coil_update)
+    obj.heatpump.subscribe(HeatPump.COIL_UPDATE_EVENT, on_coil_update)
 
     while True:
         await asyncio.sleep(1)
 
 
-@cli.command()
-@global_options
-async def product(**kwargs):
-    _, connection = await global_setup(**kwargs)
-    product_info = await connection.read_product_info()
+add_connect_command(monitor)
+
+
+@click.command()
+@click.pass_obj
+async def product(obj: ConnectionContext):
+    product_info = await obj.connection.read_product_info()
     click.echo(product_info)
 
 
-@cli.command()
-@global_options
+add_connect_command(product)
+
+
+@click.command()
+@click.pass_obj
 @click.argument("parameter", type=int)
-async def read(parameter: int, **kwargs):
-    heatpump, connection = await global_setup(**kwargs)
-
-    coil = heatpump.get_coil_by_address(parameter)
-    click.echo(await connection.read_coil(coil))
+async def read(obj: ConnectionContext, parameter: int, **kwargs):
+    coil = obj.heatpump.get_coil_by_address(parameter)
+    click.echo(await obj.connection.read_coil(coil))
 
 
-@cli.command()
-@global_options
+add_connect_command(read)
+
+
+@click.command()
+@click.pass_obj
 @click.argument("parameter", type=int)
 @click.argument("value", type=str)
-async def write(parameter: int, value: str, **kwargs):
-    heatpump, connection = await global_setup(**kwargs)
-
-    coil = heatpump.get_coil_by_address(parameter)
+async def write(obj: ConnectionContext, parameter: int, value: str, **kwargs):
+    coil = obj.heatpump.get_coil_by_address(parameter)
     if coil.mappings:
         coil.value = value
     else:
         coil.value = float(value)
-    click.echo(await connection.write_coil(coil))
+    click.echo(await obj.connection.write_coil(coil))
+
+
+add_connect_command(read)
 
 
 @cli.command()
@@ -196,5 +223,7 @@ def parse_file(file: IO):
 
 try:
     cli()
+except NibeException as exception:
+    click.echo(repr(exception))
 except (KeyboardInterrupt, SystemExit):
     pass
